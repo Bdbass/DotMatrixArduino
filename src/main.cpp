c0cfb592c9f8671608b86453fa08f4e9b07bee20
gvanhoy <user@example.com>
2017-10-11 08:31:38 -0700

    Added files

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+// Author:
+// Net ID:
+// Date:
+// Assignment:
+//
+// Description: File description
+//----------------------------------------------------------------------//
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main() {
+
+
+  while(1){
+  
+  }
+
+  return 0;
+}

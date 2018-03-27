f93bd8b00290116374394f0f247a6f3a0d053ffe
Brock <bberube@email.arizona.edu>
2018-03-27 15:28:45 -0700

    state machine implementation

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,16 @@
 #include <avr/interrupt.h>
 #include "SPI.hpp"
 #include "timer.hpp"
+#include "ShiftREG.hpp"
 
+typedef enum stateType_enum{
+  shift, wait
+} stateType;
+
+volatile stateType state = shift;
+
+volatile unsigned char data;
+volatile unsigned char counter;
 
 
 
@@ -17,15 +26,29 @@ int main() {
 
   initTimer1();
   initSPI();
+  initShiftREG();
   sei();//enable global interrupts
 
   //set slave select to low
   PORTB &= ~(1 << PORTB0); //starts the internal SPI clock
 
   //fill the SPDR register with something
-  SPDR = 'V';//just an arbitrary data value, it has no significance
+  data = 0x00;
+  SPDR = data;//just an arbitrary data value, it has no significance
 
   while(1){
+
+    switch(state){
+      case shift://update the data variable, changed in ISR
+          state = wait;
+
+          /*implement shift here and write the data*/
+
+          break;
+      case wait://waiting for the ISR
+          break;
+
+        }
 
   }
 
@@ -37,9 +60,13 @@ ISR(SPI_STC_vect){
   //set SS high
   PORTB |= (1 << PORTB0);
 
-  SPDR = 'V';//data being sent  - this data currently is useless - FIXME
+  SPDR = data;//data being sent
 
   //set SS to low
   PORTB &= ~(1 << PORTB0);
+
+
+  state = shift; //go to next state in State machine
+  counter = (counter % 8) + 1;//this counter will be used to parse through the data in the state machine 
 
 }
